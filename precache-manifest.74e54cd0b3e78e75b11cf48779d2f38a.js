f97f6897df1fb8ca211c2e6f25016e32cbcef870
JAY-kittisak <jay46789@gmail.com>
2020-09-28 13:58:29 +0700

    Updates

---
+++ precache-manifest.74e54cd0b3e78e75b11cf48779d2f38a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773ac60fa1565d2b4f35fcf40d1af549",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "394a9f1b833bec762a9f",
+    "url": "/react-project/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "92e83192271f7c20698e",
+    "url": "/react-project/static/js/2.be90933c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-project/static/js/2.be90933c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "394a9f1b833bec762a9f",
+    "url": "/react-project/static/js/main.80963ab6.chunk.js"
+  },
+  {
+    "revision": "ef0ba607d25339a84d10",
+    "url": "/react-project/static/js/runtime-main.ec1ffa47.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-project/static/media/logo.ee7cd8ed.svg"
+  }
+]);
